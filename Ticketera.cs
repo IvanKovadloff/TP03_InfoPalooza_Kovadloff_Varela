c14e59f86ed0bc9870469c4168b25c5a60c754c0
ABRIL VARELA SICA <48464274@ort>
2024-05-15 10:44:01 -0300

    avances

--- Ticketera.cs
+++ Ticketera.cs
@@ -34,10 +34,16 @@ static class Ticketera{
         
     }
     //6. Cambiar entrada
+    //CambiarEntrada: Recibe el Id de la entrada, el tipo de entrada por el cual quiere realizar el cambio y la cantidad. 
+    // Debe permitir el cambio solo cuando el importe nuevo sea superior al importe anterior de la compra. 
+    //Devuelve true o false si pudo o no realizar el cambio.
     static public bool CambiarEntrada(int id, int entrada, int cant){
         bool puedo=false;
-        bool existe=DicCliente.ContainsKey(id);
-        
+        Cliente cl=BuscarCliente(id);
+        //si el cliente no exites, devielve FALSE
+        //si el cliente existe, se fija si púee canmbiar entrada si no tira false
+        //si existe el cliente y puede cambiarlas, cambio tipo dentrada y la cantidad
+
         
         if (existe)
         {
